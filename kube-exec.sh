0ab2b77825b2653fe1632fa32bbb9a6f52b47ae0
lalyos <lalyos@yahoo.com>
2019-07-06 16:21:47 +0200

    fix namespace missmatch

--- kube-exec.sh
+++ kube-exec.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-kubectl exec -it $(kubectl get po -l run=${USER} -o jsonpath='{.items[0].metadata.name}') -- bash
+[[ "$DEBUG" ]] && set -x
+: ${NS:=$(kubectl  get deployment -l user -A -o jsonpath='{.items[0].metadata.namespace}')}
+kubectl exec -n ${NS} -it $(kubectl get po -n ${NS} -l run=${USER} -o jsonpath='{.items[0].metadata.name}') -- bash
+
+set +x
