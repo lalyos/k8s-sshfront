6eb12eeedb529a267ae57c3d0eaaf25efb84aa86
lalyos <lalyos@yahoo.com>
2019-12-20 13:41:55 +0100

    exec SSH_ORIGINAL_COMMAND if set

--- kube-exec.sh
+++ kube-exec.sh
@@ -2,6 +2,8 @@
 
 [[ "$DEBUG" ]] && set -x
 : ${NS:=$(kubectl  get deployment -l user -A -o jsonpath='{.items[0].metadata.namespace}')}
-kubectl exec -n ${NS} -it $(kubectl get po -n ${NS} -l run=${USER} -o jsonpath='{.items[0].metadata.name}') -- bash
+
+[[ "$SSH_ORIGINAL_COMMAND" ]] || OPTS='-it'
+kubectl exec -n ${NS} ${OPTS} $(kubectl get po -n ${NS} -l run=${USER} -o jsonpath='{.items[0].metadata.name}') -- ${SSH_ORIGINAL_COMMAND:=bash}
 
 set +x
